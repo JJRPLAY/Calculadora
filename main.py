072df72d0abbef583a902114cd45b23f5a397926
JJRPLAY <agostinhojuniorjive@gmail.com>
2025-07-22 19:20:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ import os
 import time
 
 def calculadora(num1: float, num2: float, operador: str) -> float:
-    """
-    Usar nan como valor inicial é uma boa prática. 
-    Se o operador fornecido não corresponder a nenhuma das opções válidas (+, -, etc.), a função retornará nan, 
-    sinalizando que o cálculo não pôde ser realizado.
-    """
     result = float("nan")
-    if operador == '+':
-        result = num1 + num2
-
-    return result
+  
+    if operador not in ['+', '-', '*', '/']:
+        print("Operador inválido. Use +, -, * ou /.\n")
+        return ("nan")
+    
+    elif operador == '-':
+        result = num1 - num2
+        return result
+    
+    elif operador == '*':
+        result = num1 * num2
+        return result
+    
+    elif operador == '/':
+        if num2 != 0:
+            result = num1 / num2
+        return result
+    else:
+        result = float("nan")
+        return result
 
 
 if __name__ == "__main__":
@@ -21,14 +32,32 @@ if __name__ == "__main__":
         try:
             print('Calculadora')
             print('----------------------------------\n')
-
-
+            
+            
+            num1 = float(input('Digite o primeiro número: '))
+            num2 = float(input('Digite o segundo número: '))
+            operando = input('Digite o operador (+, -, *, /): ')
+            
+            if operando not in ['+', '-', '*', '/']:
+                raise ValueError('NaN Operador inválido!')
+            
+            else:
+                if operando == '+':
+                    calculadora (num1, num2, operador = '+')
+                    print(f"resultado e ")
+                elif operando == '-':
+                    calculadora (num1, num2, operando = '-')
+                elif operando == '*':
+                    calculadora (num1, num2, operando = '*')
+                elif operando == '/':
+                    calculadora (num1, num2, operando = '/')
+                    
         except ValueError:
             print('Dados inválidos! -> Tente novamente!')
             time.sleep(2)
 
         except ZeroDivisionError:
             print('Impossível dividir por zero! -> Tente novamente!')
-            time.sleep(2)
-
-    print('\nVolte sempre!\n')
+            time.sleep(2) 
+    #else:
+        #print('\nVolte sempre!\n')
